6a544fc7717b26f9b626c318c5a4d9552c6fe39d
Megan Yam <119703973+megan-yam@users.noreply.github.com>
2025-01-15 16:10:03 -0800

    Started working on allergies tab

--- allergyAid/app/(tabs)/index.tsx
+++ allergyAid/app/(tabs)/index.tsx
@@ -1,8 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Text, View } from 'react-native'
 
 export default function Index() {
+  const [allergies, setAllergies] = useState([]);
+  
+  if (allergies.length === 0) {
+    return(
+      <View style={[{ flex: 1 },{ justifyContent: "center", alignItems: "center" }]}>
+        <Text>
+          No allergies listed! Press the plus button to add one.
+        </Text>
+      </View>
+    )
+  }
   return (
-    <View>allergies</View>
+    <View>
+      
+    </View>
   )
 }

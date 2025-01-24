53b0df09faebdfb2a0bc9b54509bde8021c4d2a9
Megan Yam <119703973+megan-yam@users.noreply.github.com>
2025-01-23 16:17:15 -0800

    added react native camera package

--- allergyAid/app/(tabs)/camera.tsx
+++ allergyAid/app/(tabs)/camera.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import { Camera, useCameraDevice } from 'react-native-vision-camera'
 
 export default function camera() {
+  const device = useCameraDevice('back')
+
+  // if (device == null) return <NoCamera />
   return (
-    <View>
-        
-    </View>
-  )
+    <Camera
+      // style={StyleSheet.absoluteFill}
+      device={device}
+      isActive={true}
+    />
 }
--- allergyAid/app/(tabs)/index.tsx
+++ allergyAid/app/(tabs)/index.tsx
@@ -2,7 +2,16 @@ import React, { useState } from 'react'
 import { Text, View } from 'react-native'
 
 export default function Index() {
-  const [allergies, setAllergies] = useState([]);
+  const [allergies, setAllergies] = useState([""]);
+  const [menuVisible, showMenu] = useState(false);
+
+  const push = (allergy: string) => {
+    setAllergies([allergy,...allergies]);
+  }
+
+  const toggleMenu = () => {
+    showMenu(!menuVisible);
+  }
   
   if (allergies.length === 0) {
     return(
